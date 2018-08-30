cbcd153a49165d4b468f877e95171ee7b1465e64
salemgrace <salem.bryant@gmail.com>
2018-08-30 17:30:44 -0400

    pseudo-code

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// Create a welcome page - ready to begin?
+
+// Create a series of questions
+
+// In each question offer 4 selections
+
+// In each question "mark" an answer as right or wrong
+
+// There needs to be a correct or incorrect indicator
+
+// keep tally of the progress at the bottom
+
+// decide what percentage wins/loses
+
+// Change page to announce winner/loser at the end
+
