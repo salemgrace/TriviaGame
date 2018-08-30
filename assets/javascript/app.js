bdc5367414f7e52b430046f3e0496872c75f0a50
salemgrace <salem.bryant@gmail.com>
2018-08-30 18:31:56 -0400

    class ready

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,76 @@
+// Create a series of questions
+var questions = [
+    {
+        questionOne: "What is Salem's favorite color?",
+        answers: {
+            a: "Black",
+            b: "Blue",
+            c: "Green",
+            d: "Pink",
+        },
+
+        correctAnswerOne: "a"
+    },
+    {
+        questionTwo: "What was the name of Salem's first pet?",
+        answers: {
+            a: "Olive",
+            b: "Annabelle",
+            c: "Marvin",
+            d: "Corky",
+        },
+
+        correctAnswerTwo: "b"
+    }
+]
+
 // Create a welcome page - ready to begin?
 
-// Create a series of questions
+function triviaQuiz () {
+    questions.forEach( (currentQuestion, questionNumber) => {
 
-// In each question offer 4 selections
+    });
+}
+
+
+window.onload = function() {
+    $("#readyButton").on("click", stopWatch.newQuestion);
+  };
+
+var intervalId;
+
+var stopWatch = {
+    time: 0,
+
+    newQuestion: function() {
+        stopWatch.time = 1000 * 20;
+        $("#timer").text("00:20");
+        intervalId = setInterval(stopWatch.count, -1000);
+    },
+
+    answerClicked: function() {
+        clearInterval(intervalId);
+        timerRunning = false;
+    },
+
+    count: function() {
+        stopWatch.time--;
+
+        var converted = stopWatch.timeConverter(stopWatch.time);
+        console.log(converted);
+        $("#timer").text(converted);
+    },
+
+    timeConverter: function(t) {
+        var seconds = Math.floor(t);
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        return seconds;
+    }
+}
 
 // In each question "mark" an answer as right or wrong
 
